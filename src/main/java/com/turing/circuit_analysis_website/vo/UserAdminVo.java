f5ae37f2a7aab80e893de422fafe92fdb307c8bb
Chen-xh <1132728898@qq.com>
2021-01-11 19:39:50 +0800

    局部修改

---
+++ src/main/java/com/turing/circuit_analysis_website/vo/UserAdminVo.java
@@ -0,0 +1,45 @@
+package com.turing.circuit_analysis_website.vo;
+
+import com.turing.circuit_analysis_website.validator.IsStudentNo;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+
+/**
+ * @author CHEN
+ * @date 2020/10/12  17:34
+ */
+@Data
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserAdminVo implements Serializable {
+    private Long id;
+    @NotEmpty(message = "学号不能为空")
+    @Size(max = 12, min = 6, message = "请输入12位正确的学号或6位教工号")
+    private String username;
+    @NotEmpty(message = "密码不能为空")
+    @Size(max = 16, min = 3, message = "请输入3-16位正确的密码")
+    private String password;
+    //性别
+    @NotEmpty(message = "学院不能为空")
+    private String gender;
+    //学院
+    @NotEmpty(message = "学院不能为空")
+    private String college;
+    //专业
+    @NotEmpty(message = "专业不能为空")
+    private String major;
+    //班级
+    @NotEmpty(message = "班级不能为空")
+    private String user_class;
+    //姓名
+    @NotEmpty(message = "姓名不能为空")
+    private String name;
+
+}
